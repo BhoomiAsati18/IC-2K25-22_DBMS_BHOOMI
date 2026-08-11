a8bc22be0f710ef1d974a80a9d789fad1dda0850
Bhoomi Asati <asatibhoomi18@gmail.com>
2026-08-11 12:18:21 +0530

    Create countries table and insert initial data
    Create countries table if it exist already.

---
+++ countries_if_exist.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS countries;
+CREATE TABLE countries(
+  country_id INT,
+  country_name VARCHAR(50),
+  region_id INT
+);
+
+INSERT INTO countries(country_id, country_name, region_id) VALUES
+(1, 'India', 101),
+(2, 'U.S.', 102),
+(3, 'U.K.', 103);
+
+
+SELECT * FROM countries;
